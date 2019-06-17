c80e9c77b39eb41ad8fa1dcb6e6c9c352286aaa0
jiw065 <wwjjjj0923@gmail.com>
2019-06-17 12:38:40 -0400

    add

---
+++ Leetcode/src/CyclicRotation.java
@@ -0,0 +1,30 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class CyclicRotation {
+	public int[] solution(int[] A, int K) {
+        // write your code in Java SE 8
+        if (A.length == 0){
+            return A; 
+        }
+        
+        Deque<Integer> q = new LinkedList<Integer>();
+        for(int i =A.length-1; i>=0 ;i--){
+            q.addFirst(A[i]);
+        }
+        int i = K;
+        while(i>0 ){
+            Integer num = q.removeLast();
+            q.addFirst(num); 
+            i--;
+        }
+        int[] B = new int[A.length];
+        int j = 0;
+        for(int k: q ){
+            B[j] = k;
+            j++;
+        }
+        
+        return B;
+    }
+}
