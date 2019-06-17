725508331a2a18b33d6ac954c41de35311a725eb
jiw065 <wwjjjj0923@gmail.com>
2019-06-16 23:23:24 -0400

    add

---
+++ Leetcode/src/CountFactors.java
@@ -0,0 +1,23 @@
+
+public class CountFactors {
+	 public int solution(int N) {
+	        // write your code in Java SE 8
+	        if (N == 1 ){
+	            return 1; 
+	        }
+	        int i = 1;
+	        int count = 0;
+	        while(i*i<=N){
+	            if (N%i == 0){
+	                if (i == N/i){
+	                    count += 1;
+	                }else{
+	                     count += 2;
+	                }
+	            }
+	           
+	             i++;
+	        }
+	        return count; 
+	    }
+}
