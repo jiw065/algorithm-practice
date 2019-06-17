87b25adfc0731cd5a308511d9b2f07c5dac7f4b7
jiw065 <wwjjjj0923@gmail.com>
2019-06-16 23:34:02 -0400

    add

---
+++ Leetcode/src/MinPerimeterRectangle.java
@@ -0,0 +1,22 @@
+
+public class MinPerimeterRectangle {
+	public int solution(int N) {
+        // write your code in Java SE 8
+        
+        int a = 1;
+        int b = 0;  
+        int min = Integer.MAX_VALUE;
+        int per = 0; 
+        while(a*a <= N){
+            if(N%a == 0){
+                b = N/a;
+                per = 2*(a+b);
+                min = Math.min(min, per);
+                
+            }
+            a++;
+        }
+        
+        return min; 
+    }
+}
