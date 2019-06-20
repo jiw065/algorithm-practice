3035dd98f0628747dadadd650d36626f184e6be7
jiw065 <wwjjjj0923@gmail.com>
2019-06-20 02:40:01 -0400

    update

---
+++ Leetcode/src/IsAnagram.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class IsAnagram {
+	  public boolean isAnagram(String s, String t) {
+
+	        char[] sc = s.toCharArray();
+	        char[] tc = t.toCharArray();
+	        if (s.length() != t.length()){
+	            return false; 
+	        }
+	        Arrays.sort(sc);
+	        Arrays.sort(tc);
+	        
+	        for (int i =0; i<sc.length;i++){
+	            if (sc[i] != tc[i]){
+	                return false;
+	            }
+	        }
+	        return true;
+	    }
+}
