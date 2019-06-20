d6b45dc16e7f667dc56c5c4728c1fdb0115f8d0e
jiw065 <wwjjjj0923@gmail.com>
2019-06-19 22:54:33 -0400

    update need to focus on

---
+++ Leetcode/src/StarReverseLinkedList.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class StarReverseLinkedList {
+	 public ListNode reverseList(ListNode head) {
+	        if(head == null || head.next == null){
+	            return head;
+	        }
+	        
+	        ListNode node =  reverseList(head.next);
+	        head.next.next = head;
+	        head.next = null;
+	        return node; 
+	            
+	        
+	    }
+}
