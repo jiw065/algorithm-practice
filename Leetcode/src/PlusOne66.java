587ec4f273eb01fe2a49fc2e723774d1c5059368
jiw065 <wwjjjj0923@gmail.com>
2019-06-22 19:33:27 -0400

    update

--- Leetcode/src/PlusOne66.java
+++ Leetcode/src/PlusOne66.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.stream.Collectors;
 
@@ -22,5 +23,33 @@ public class PlusOne66 {
 	            }
 	        }
 	        return num.stream().mapToInt(i->i).toArray();
+	    }
+	 
+	 
+	 public int[] plusOne2(int[] digits) {
+	       LinkedList<Integer> num = new LinkedList<>();
+
+	        int head = digits[0];
+	        int[] copy = null;
+	        for(int i=digits.length-1 ;i>=0; i--){
+	           
+	            if(digits[i] != 9){
+	             digits[i] = digits[i]+1; 
+	                break;
+	            }else{
+	             digits[i] = 0;
+	            }
+	            
+	            if(digits[0] == 0 && head != 0){
+	               copy = new int[digits.length+1];
+	               copy[0] = 1;
+	               for (int j =0 ;j<digits.length;j++){
+	                   copy[j+1] = 0; 
+	               }
+	                
+	                
+	            }
+	        }
+	        return copy !=null?copy:digits;
 	    }
 }
